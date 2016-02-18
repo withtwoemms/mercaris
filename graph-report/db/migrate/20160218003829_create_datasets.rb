1f31c48c4e6592c0ec338e3d5096206dac117a44
Emmanuel I. Obi <withtwoemms@gmail.com>
2016-02-17 18:59:57 -0600

    set the table

--- graph-report/db/migrate/20160218003829_create_datasets.rb
+++ graph-report/db/migrate/20160218003829_create_datasets.rb
@@ -1,7 +1,11 @@
 class CreateDatasets < ActiveRecord::Migration
   def change
     create_table :datasets do |t|
-
+      t.string :contract
+      t.date :start_time
+      t.date :end_time
+      t.string :source
+      t.decimal :price
       t.timestamps null: false
     end
   end
