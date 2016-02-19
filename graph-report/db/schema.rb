94081d7ef8becac4479fa7c5f182311f94ffb2ed
Emmanuel I. Obi <withtwoemms@gmail.com>
2016-02-18 18:42:37 -0600

    solid migration

--- graph-report/db/schema.rb
+++ graph-report/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160218234602) do
+ActiveRecord::Schema.define(version: 20160218235820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20160218234602) do
   create_table "datasets", force: :cascade do |t|
     t.string   "contract"
     t.string   "start_time"
-    t.date     "end_time"
+    t.string   "end_time"
     t.string   "source"
     t.decimal  "price"
     t.datetime "created_at", null: false
